be63668e25716bae97397db9ce2563467d9dec1f
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-08-19 23:14:04 +0200

    added "new" command to initalize projects @cli

--- packages/cli/src/main.rs
+++ packages/cli/src/main.rs
@@ -1,40 +1,61 @@
-use std::fs::{create_dir_all, write};
+mod files;
+use std::{
+    fs::{create_dir_all, write},
+    process::Command,
+};
 
 use clap::{command, Parser, Subcommand};
 
 #[derive(Parser)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Command>,
+    command: Option<Commands>,
 }
 
 #[derive(Subcommand)]
-enum Command {
+enum Commands {
     New { dir: Option<String> },
 }
 
-fn initialize_project(path: String) -> Result<(), std::io::Error> {
-    write(path, "Done")
+fn initialize_project(path: &str) -> Result<(), std::io::Error> {
+    let index_path = format!("{}/index.ts", path);
+    let core_path = format!("{}/core.ts", path);
+
+    write(index_path, files::INDEX_CONTENT)?;
+    write(core_path, files::CORE_CONTENT)
 }
 
-fn handle_new_command(name: Option<String>) -> Result<(), std::io::Error> {
-    let path = name.unwrap_or("./".to_string());
+fn handle_new_command(name: Option<&str>) -> Result<(), std::io::Error> {
+    let path = name.unwrap_or(".");
 
-    let dir_exists = std::path::Path::new(&path).exists();
+    let dir_exists = std::path::Path::new(path).exists();
 
     if !dir_exists {
-        create_dir_all(&path)?;
+        create_dir_all(path)?;
     }
 
-    initialize_project(path + "/index.ts")
+    initialize_project(path)?;
+    Command::new("bun")
+        .arg("init")
+        .arg("-y")
+        .current_dir(path)
+        .spawn()?;
+
+    Command::new("bun")
+        .arg("add")
+        .arg("@bunny-ts/core")
+        .current_dir(path)
+        .spawn()?;
+    Ok(())
 }
 
 fn main() {
     let matches = Cli::parse();
 
     match matches.command {
-        Some(Command::New { dir }) => {
-            handle_new_command(dir).expect("Failed to create new project");
+        Some(Commands::New { dir }) => {
+            let path = dir.as_deref();
+            handle_new_command(path).expect("Failed to create new project");
         }
         None => println!("No command provided"),
     }
