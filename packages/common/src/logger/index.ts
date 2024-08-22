f1ba0bc84a7b451da98a810123a813e914dbf28f
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-08-22 22:19:24 +0200

    added basic logger functionality

---
+++ packages/common/src/logger/index.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@bunny-ts/core';
+@Injectable()
+export class Logger {
+  public static success(message: string): void {
+    console.log(`\x1b[32m [✅] ${new Date().toLocaleTimeString()} ---> \x1b[0m ${message} `);
+  }
+  public static error(message: string): void {
+    console.log(`\x1b[31m [❌] ${new Date().toLocaleTimeString()} ---> \x1b[0m ${message} `);
+  }
+}
